42d2309cec72654be57ef773266f60396ba0e2bf
Jesus Ale <jesus.9314@gmail.com>
2021-10-28 16:46:09 -0500

    Se agregó el archivo dt_es.json y se modificó functions_categorias.js a enmascript 6

--- Assets/js/functions_categorias.js
+++ Assets/js/functions_categorias.js
@@ -1,241 +1,21 @@
-//declaración de letiables
-let divLoading = document.querySelector("#divLoading"); //loading
-let tableCategorias; //tabla usuarios
+'use strict'
 
-function openModal()
-{
-	document.querySelector('#idCategoria').value="";
-	document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
-	document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
-	document.querySelector('#btnText').innerHTML = "Guardar";
-	document.querySelector('#titleModal').innerHTML = "Nueva Categoria";
-	document.querySelector("#formCategoria").reset();
-	$('#modalFormCategorias').modal('show');
-}
-
-function removePhoto(){
-	    document.querySelector('#foto').value ="";
-	    document.querySelector('.delPhoto').classList.add("notBlock");
-	    document.querySelector('#img').remove();
-	}
-
-document.addEventListener('DOMContentLoaded', function(){
-
-	tableCategorias = $('#tableCategorias').dataTable({
-        "scrollX":true,
-        "stateSave":true,
-        "aProcessing":true,
-        "aServerSide":true,
-        "stateSave": true,
-        "fixedHeader": true,
-        "language": 
-        {
-            "sProcessing":"Procesando...",
-            "sLengthMenu":"Mostrar _MENU_ registros",
-            "sZeroRecords":"No se encontraron resultados",
-            "sEmptyTable":"Ningún dato disponible en esta tabla =(",
-            "sInfo":"Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "sInfoEmpty":"Mostrando registros del 0 al 0 de un total de 0 registros",
-            "sInfoFiltered":"(filtrado de un total de _MAX_ registros)",
-            "sInfoPostFix":"",
-            "sSearch":"Buscar:",
-            "sUrl":"",
-            "sInfoThousands":",",
-            "sLoadingRecords":"Cargando...",
-            "oPaginate": 
-            {
-                "sFirst":"Primero",
-                "sLast":"Último",
-                "sNext":"Siguiente",
-                "sPrevious":"Anterior"
-            },
-                        "oAria": {
-                            "sSortAscending":  ": Actilet para ordenar la columna de manera ascendente",
-                            "sSortDescending": ": Actilet para ordenar la columna de manera descendente"
-                        },
-                        "buttons": {
-                            "copy": "Copiar",
-                            "colvis": "Visibilidad"
-                        }
-        },
-        "ajax":
-        {
-            "url": " "+base_url+"/Categorias/getCategorias",
-            "dataSrc":""
-        },
-        "columns":
-        [
-            {"data":"idcategoria", "width": "5%", "className": "text-center"},
-            {"data":"nombre", "width": "10%"},
-            {"data":"descripcion"},
-            {"data":"status", "width": "5%", "className": "text-center", render : function(data)
-            {
-            	if(data == 1)
-            	{
-            		return '<span class="badge badge-success">Activo</span>';
-            	}
-            	else
-            	{
-            		return '<span class="badge badge-danger">Inactivo</span>';
-            	}
-            }
-
-        },
-            {"data":"options" , "width":"10%"}
-        ],
-        "dom": 'lBfrtip',
-        "buttons": [
-            {
-            	"extend": "copyHtml5",
-            	"text":"<i class='far fa-copy'></i> Copiar",
-            	"titleAttr": "Copiar",
-            	"className": "btn-sm btn-secondary",
-            	"title":"tabla_categorias",
-            	"exportOptions":{
-            		"columns": [ 0, 1, 2, 3]
-            	}
-            },
-            {
-            	"extend": "excelHtml5",
-            	"text":"<i class='far fa-file-excel'></i> Excel",
-            	"titleAttr": "Exportar a Excel",
-            	"className": "btn-sm btn-success",
-            	"title":"tabla_categorias",
-            	"exportOptions":{
-            		"columns": [ 0, 1, 2, 3]
-            	}
-            },
-            {
-            	"extend": "pdfHtml5",
-            	"text":"<i class='far fa-file-pdf'></i> PDF",
-            	"titleAttr": "Exportar a PDF",
-            	"className": "btn-sm btn-danger",
-            	"messageTop":"Tabla de usuarios",
-            	"title":"tabla_categorias",
-            	"exportOptions":{
-            		"columns": [ 0, 1, 2, 3]
-            	}
-            },
-            {
-            	"extend": "csvHtml5",
-            	"text":"<i class='fas fa-file-csv'></i> CSV",
-            	"titleAttr": "Exportar a CSV",
-            	"className": "btn-sm btn-info",
-            	"title":"tabla_categorias",
-            	"exportOptions":{
-            		"columns": [ 0, 1, 2, 3]
-            	}
-            }
-        ],
-        "resonsieve":"true",
-        "bDestroy": true,
-        "iDisplayLength": 10,
-        "order":[[0,"asc"]] 
-    });
-
-	if(document.querySelector("#foto"))
-	{
-    let foto = document.querySelector("#foto");
-	    foto.onchange = function(e) 
-	    {
-	        let uploadFoto = document.querySelector("#foto").value;
-	        let fileimg = document.querySelector("#foto").files;
-	        let nav = window.URL || window.webkitURL;
-	        let contactAlert = document.querySelector('#form_alert');
-	        if(uploadFoto !='')
-	        {
-	            let type = fileimg[0].type;
-	            let name = fileimg[0].name;
-	            if(type != 'image/jpeg' && type != 'image/jpg' && type != 'image/png')
-	            {
-	                contactAlert.innerHTML = '<p class="errorArchivo">El archivo no es válido.</p>';
-	                if(document.querySelector('#img')){
-	                    document.querySelector('#img').remove();
-	                }
-	                document.querySelector('.delPhoto').classList.add("notBlock");
-	                foto.value="";
-	                return false;
-	            }
-	            else
-	            {  
-	                    contactAlert.innerHTML='';
-	                    if(document.querySelector('#img'))
-	                    {
-	                        document.querySelector('#img').remove();
-	                    }
-	                    document.querySelector('.delPhoto').classList.remove("notBlock");
-	                    let objeto_url = nav.createObjectURL(this.files[0]);
-	                    document.querySelector('.prevPhoto div').innerHTML = "<img id='img' src="+objeto_url+">";
-	            }
-	        }else{
-	            alert("No selecciono foto");
-	            if(document.querySelector('#img')){
-	                document.querySelector('#img').remove();
-	            }
-	        }
-	    }
-	}
-
-	if(document.querySelector(".delPhoto")){
-	    let delPhoto = document.querySelector(".delPhoto");
-	    delPhoto.onclick = function(e) {
-	        removePhoto();
-	    }
-	}
-
-	 //Nueva Categoría
-    let formCategoria = document.querySelector("#formCategoria");
-    formCategoria.onsubmit = function(e) {
-        e.preventDefault();
-
-        let intIdRol = document.querySelector('#idCategoria').value;
-        let strNombre = document.querySelector('#txtNombre').value;
-        let strDescripcion = document.querySelector('#txtDescripcion').value;
-        let intStatus = document.querySelector('#listStatus').value;        
-        if(strNombre == '' || strDescripcion == '' || intStatus == '')
-        {
-            swal("Atención", "Todos los campos son obligatorios." , "error");
-            return false;
-        }
-        divLoading.style.display ="flex"
-        let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-        let ajaxUrl = base_url+'/Categorias/setCategoria'; 
-        let formData = new FormData(formCategoria);
-        request.open("POST",ajaxUrl,true);
-        request.send(formData);
-        request.onreadystatechange = function(){
-           if(request.readyState == 4 && request.status == 200){
-                
-                let objData = JSON.parse(request.responseText);
-                if(objData.status)
-                {
-                    $('#modalFormCategorias').modal("hide");
-                    formCategoria.reset();
-                    swal("Categorías", objData.msg ,"success");
-                    removePhoto();
-                    tableCategorias.api().ajax.reload(null,false);
-                }else{
-                    swal("Error", objData.msg , "error");
-                }              
-            }
-            divLoading.style.display = "none";
-            return false; 
-        }
-
-        
-    }
-
-},false);
-
-function fntViewInfo(idcategoria){
+//declaración de variables
+let divLoading = document.querySelector("#divLoading");
+let tableCategorias;
+//
+//
+//objeto CATEGORIAS
+var categorias = {
+  ViewInfo: (idcategoria) => {
     let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     let ajaxUrl = base_url+'/Categorias/getCategoria/'+idcategoria;
     request.open("GET",ajaxUrl,true);
     request.send();
     request.onreadystatechange = function(){
         if(request.readyState == 4 && request.status == 200){
-            let objData = JSON.parse(request.responseText);
-            if(objData.status)
+          let objData = JSON.parse(request.responseText);
+          if(objData.status)
             {
                 let estado = objData.data.status == 1 ? 
                 '<span class="badge badge-success">Activo</span>' : 
@@ -251,10 +31,8 @@ function fntViewInfo(idcategoria){
             }
         }
     }
-}
-
-function fntEditInfo(element,idcategoria){
-
+  },  
+  EditInfo: (element,idcategoria) => {
     document.querySelector('#titleModal').innerHTML ="Actualizar Categoría";
     document.querySelector('.modal-header').classList.replace("headerRegister", "headerUpdate");
     document.querySelector('#btnActionForm').classList.replace("btn-primary", "btn-info");
@@ -300,9 +78,8 @@ function fntEditInfo(element,idcategoria){
             }
         }
     }
-}
-
-function fntDelInfo(idcategoria){
+  },
+  DelInfo: (idcategoria) => {
     swal({
         title: "Eliminar Categoría",
         text: "¿Realmente quiere eliminar al categoría?",
@@ -312,8 +89,7 @@ function fntDelInfo(idcategoria){
         cancelButtonText: "No, cancelar!",
         closeOnConfirm: false,
         closeOnCancel: true
-    }, function(isConfirm) {
-        
+    }, (isConfirm) => {
         if (isConfirm) 
         {
             let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
@@ -335,11 +111,214 @@ function fntDelInfo(idcategoria){
                 }
             }
         }
-
     });
+  }
+};
+//fin CATEGORIAS
+//
+//
+//objeto PAGE
+var page = {
+  openModal: () => {
+    document.querySelector('#idCategoria').value="";
+    document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
+    document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
+    document.querySelector('#btnText').innerHTML = "Guardar";
+    document.querySelector('#titleModal').innerHTML = "Nueva Categoria";
+    document.querySelector("#formCategoria").reset();
+    $('#modalFormCategorias').modal('show'); 
+  },
+  removePhoto: () => {
+    document.querySelector('#foto').value ="";
+	  document.querySelector('.delPhoto').classList.add("notBlock");
+	  document.querySelector('#img').remove();
+  }
+};
+//fin PAGE
+//
+//
+//funciones al cargar el DOM
+document.addEventListener('DOMContentLoaded', () => {
+  
+  //inicializamos el DATATABLE
+  tableCategorias = $('#tableCategorias').dataTable({
+        "scrollX":true,
+        "stateSave":true,
+        "aProcessing":true,
+        "stateSave": true,
+        "fixedHeader": true,
+        "resonsieve":"true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "dom":'lBfrtip',
+        "order":[[0,"asc"]], 
+        "language": 
+        {
+            url: " "+base_url+"/Assets/js/config/dt_es.json"
+        },
+        "ajax":
+        {
+            "url": " "+base_url+"/Categorias/getCategorias",
+            "dataSrc":""
+        },
+        "columns":
+        [
+            {"data":"idcategoria", "width": "5%", "className": "text-center"},
+            {"data":"nombre", "width": "10%"},
+            {"data":"descripcion"},
+            {"data":"status", "width": "5%", "className": "text-center", 
+               render : (data) =>
+                {
+                  if(data == 1)
+                  {
+                    return '<span class="badge badge-success">Activo</span>';
+                  }
+                  else
+                  {
+                    return '<span class="badge badge-danger">Inactivo</span>';
+                  }
+              }
 
-}
-
-/*setInterval(function () {
-    tableCategorias.api().ajax.reload(null,false);
-    }, 3000 );*/
+            },
+            {"data":"options" , "width":"10%"}
+        ],
+        "buttons": [
+            {
+            	"extend": "copyHtml5",
+            	"text":"<i class='far fa-copy'></i> Copiar",
+            	"titleAttr": "Copiar",
+            	"className": "btn-sm btn-secondary",
+            	"title":"tabla_categorias",
+            	"exportOptions":{
+            		"columns": [ 0, 1, 2, 3]
+            	}
+            },
+            {
+            	"extend": "excelHtml5",
+            	"text":"<i class='far fa-file-excel'></i> Excel",
+            	"titleAttr": "Exportar a Excel",
+            	"className": "btn-sm btn-success",
+            	"title":"tabla_categorias",
+            	"exportOptions":{
+            		"columns": [ 0, 1, 2, 3]
+            	}
+            },
+            {
+            	"extend": "pdfHtml5",
+            	"text":"<i class='far fa-file-pdf'></i> PDF",
+            	"titleAttr": "Exportar a PDF",
+            	"className": "btn-sm btn-danger",
+            	"messageTop":"Tabla de usuarios",
+            	"title":"tabla_categorias",
+            	"exportOptions":{
+            		"columns": [ 0, 1, 2, 3]
+            	}
+            },
+            {
+            	"extend": "csvHtml5",
+            	"text":"<i class='fas fa-file-csv'></i> CSV",
+            	"titleAttr": "Exportar a CSV",
+            	"className": "btn-sm btn-info",
+            	"title":"tabla_categorias",
+            	"exportOptions":{
+            		"columns": [ 0, 1, 2, 3]
+            	}
+            }
+        ]
+}, false)//fin DATATABLE
+  
+  //Upload FOTO
+  if(document.querySelector("#foto"))
+	{
+    let foto = document.querySelector("#foto");
+	    foto.onchange = function(e) 
+	    {
+	        let uploadFoto = document.querySelector("#foto").value;
+	        let fileimg = document.querySelector("#foto").files;
+	        let nav = window.URL || window.webkitURL;
+	        let contactAlert = document.querySelector('#form_alert');
+	        if(uploadFoto !='')
+	        {
+	            let type = fileimg[0].type;
+	            let name = fileimg[0].name;
+	            if(type != 'image/jpeg' && type != 'image/jpg' && type != 'image/png')
+	            {
+	                contactAlert.innerHTML = '<p class="errorArchivo">El archivo no es válido.</p>';
+	                if(document.querySelector('#img')){
+	                    document.querySelector('#img').remove();
+	                }
+	                document.querySelector('.delPhoto').classList.add("notBlock");
+	                foto.value="";
+	                return false;
+	            }
+	            else
+	            {  
+	                    contactAlert.innerHTML='';
+	                    if(document.querySelector('#img'))
+	                    {
+	                        document.querySelector('#img').remove();
+	                    }
+	                    document.querySelector('.delPhoto').classList.remove("notBlock");
+	                    let objeto_url = nav.createObjectURL(this.files[0]);
+	                    document.querySelector('.prevPhoto div').innerHTML = "<img id='img' src="+objeto_url+">";
+	            }
+	        }else{
+	            alert("No selecciono foto");
+	            if(document.querySelector('#img')){
+	                document.querySelector('#img').remove();
+	            }
+	        }
+	    }
+	}
+  //fin upload FOTO
+  //
+  //
+  //Remover FOTO
+	if(document.querySelector(".delPhoto")){
+	    let delPhoto = document.querySelector(".delPhoto");
+	    delPhoto.onclick = (e) => {
+	        page.removePhoto();
+	    }
+	}
+  //fin remover FOTO
+  //
+  //
+	//Nueva Categoría
+    let formCategoria = document.querySelector("#formCategoria");
+    formCategoria.onsubmit = (e) => {
+        e.preventDefault();
+        let intIdRol = document.querySelector('#idCategoria').value;
+        let strNombre = document.querySelector('#txtNombre').value;
+        let strDescripcion = document.querySelector('#txtDescripcion').value;
+        let intStatus = document.querySelector('#listStatus').value;        
+        if(strNombre == '' || strDescripcion == '' || intStatus == '')
+        {
+            swal("Atención", "Todos los campos son obligatorios." , "error");
+            return false;
+        }
+        divLoading.style.display ="flex"
+        let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        let ajaxUrl = base_url+'/Categorias/setCategoria'; 
+        let formData = new FormData(formCategoria);
+        request.open("POST",ajaxUrl,true);
+        request.send(formData);
+        request.onreadystatechange = function(){
+           if(request.readyState == 4 && request.status == 200){
+                let objData = JSON.parse(request.responseText);
+                if(objData.status)
+                {
+                    $('#modalFormCategorias').modal("hide");
+                    formCategoria.reset();
+                    swal("Categorías", objData.msg ,"success");
+                    page.removePhoto();
+                    tableCategorias.api().ajax.reload(null,false);
+                }else{
+                    swal("Error", objData.msg , "error");
+                }              
+            }
+            divLoading.style.display = "none";
+            return false; 
+        } 
+    }
+  //fin nueva categoria
+});
